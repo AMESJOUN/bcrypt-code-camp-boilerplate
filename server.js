e3c144fb057304a38ae96e4e862559512122955d
SkizoFull <skizofull@Skizo>
2025-10-09 07:47:12 +0200

    Hash and Compare Passwords Asynchronously

--- server.js
+++ server.js
@@ -16,11 +16,10 @@ app.get('/',(req,res) => {
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+const hasg = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
   console.log(hash);
-  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-  bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
-    console.log(res); //true
+   bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res); 
   });
 });
 
